4d61d4cd8ef66e8af9cf292aa966b5f538160370
hasanzaidi1 <hasanammarza@usf.edu>
2024-08-21 22:36:05 -0400

    biceps.py added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pyperclip  # Import pyperclip for clipboard operations
 from exercises.pullDay import PullDay  # Import the PullDay class from pullDay.py
 from exercises.pushDay import PushDay
 from exercises.legDay import LegsWorkout
+from exercises.biceps import Biceps
 
 class Application:
     def __init__(self):
@@ -61,7 +62,7 @@ class Application:
         today = datetime.datetime.now().strftime("%A")
         workout = self.get_todays_WO_plan()
 
-        # Check if today is Pull Day
+
         if workout == "Pull Day (Back)":
             workout_message = (f"Good morning! Today is {today}, and your workout is:{workout} \n\n"
                                f"{self.get_pull_day_plan()}")
@@ -71,6 +72,9 @@ class Application:
         elif workout == "Legs":
             workout_message = (f"Good morning! Today is {today}, and your workout is:{workout} \n\n"
                                f"{self.get_legs_day_plan()}")
+        elif workout == "Biceps":
+            workout_message = (f"Good morning! Today is {today}, and your workout is:{workout} \n\n"
+                               f"{self.get_biceps_day_plan()}")
         else:
             workout_message = f"Good morning! Today is {today}, and your workout is: {workout}"
 
@@ -110,7 +114,10 @@ class Application:
 
     # Get Biceps Day workout plan
     def get_biceps_day_plan(self):
-        pass
+        workout = Biceps()
+        plan = workout.bicepDay()
+        plan_message = "\n".join([f"{key}: {value}" for key, value in plan.items()])
+        return plan_message
 
     def get_legs_day_plan(self):
         workout = LegsWorkout()
